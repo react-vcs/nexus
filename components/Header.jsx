16fe05802a71827228b0adb3ddc9db04d89483ef
Khalif AL Mahmud <khalif@Khalifs-MBP.netis>
2024-01-28 17:37:15 +0600

    fix nav menu on mobile

--- components/Header.jsx
+++ components/Header.jsx
@@ -46,16 +46,17 @@ const Header = (props) => {
       <nav className="bg-black py-4 border-b border-gray-50">
         <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
           <div className="relative flex h-16 items-center justify-between">
+            {/* Mobile menu Button Start */}
             <div
               className="absolute inset-y-0 left-0 flex items-center sm:hidden"
               // onClick={toggleMobileNavMenu}
-              onTouchStart={toggleMobileNavMenu}
+              onClick={toggleMobileNavMenu}
             >
               <button
                 type="button"
                 className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
                 aria-controls="mobile-menu"
-                aria-expanded="false"
+                aria-expanded="true"
               >
                 <span className="absolute -inset-0.5"></span>
                 <span className="sr-only">Open main menu</span>
@@ -90,6 +91,7 @@ const Header = (props) => {
                 </svg>
               </button>
             </div>
+            {/* Mobile Menu Button End  */}
             <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
               <div className="flex flex-shrink-0 items-center">
                 <Link href={"/"}>
@@ -187,7 +189,7 @@ const Header = (props) => {
               </div>
               {/* Search End  */}
               <div className="relative ml-3">
-                <div onTouchStart={toggleUserMenu}>
+                <div onClick={toggleUserMenu}>
                   <button
                     type="button"
                     className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
@@ -260,11 +262,9 @@ const Header = (props) => {
           </div>
         </div>
 
-        <div
-          className={`${mobileNavMenu ? "block" : "none"} `}
-          id="mobile-menu"
-        >
-          <div className="space-y-1 px-2 pb-3 pt-2">
+        {/* Start Mobile Menu  */}
+        {mobileNavMenu === false ? null : (
+          <div className="space-y-1 px-2 pb-3 pt-2 md:hidden">
             {categories?.map((menu, index) => {
               return (
                 <Link
@@ -279,7 +279,7 @@ const Header = (props) => {
               );
             })}
           </div>
-        </div>
+        )}
       </nav>
     </>
   );
