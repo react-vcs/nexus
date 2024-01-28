d02ab98a265773a8eec8dc485486d30289c97cae
Khalif AL Mahmud <khalif@Khalifs-MBP.netis>
2024-01-28 17:45:03 +0600

    fix profile nav in mobile

--- components/Header.jsx
+++ components/Header.jsx
@@ -193,9 +193,6 @@ const Header = (props) => {
                   <button
                     type="button"
                     className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                    id="user-menu-button"
-                    aria-expanded="false"
-                    aria-haspopup="true"
                   >
                     <span className="absolute -inset-1.5"></span>
                     <span className="sr-only">Open user menu</span>
@@ -208,55 +205,36 @@ const Header = (props) => {
                     />
                   </button>
                 </div>
+                {/* Profile Menu Start  */}
+                {userMenu === false ? null : (
+                  <div
+                    className={`absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none`}
+                  >
+                    {/* <!-- Active: "bg-gray-100", Not Active: "" -->  */}
+                    <Link
+                      href="/"
+                      className="block px-4 py-2 text-sm text-gray-700"
+                      role="menuitem"
+                      tabIndex="-1"
+                    >
+                      Your Profile
+                    </Link>
+                    <Link
+                      href="/"
+                      className="block px-4 py-2 text-sm text-gray-700"
+                    >
+                      Settings
+                    </Link>
+                    <Link
+                      href="/"
+                      className="block px-4 py-2 text-sm text-gray-700"
+                    >
+                      Sign out
+                    </Link>
+                  </div>
+                )}
 
-                {/* <!--
-            Dropdown menu, show/hide based on menu state. */}
-
-                {/* Entering: "transition ease-out duration-100"
-              From: "transform opacity-0 scale-95"
-              To: "transform opacity-100 scale-100"
-            Leaving: "transition ease-in duration-75"
-              From: "transform opacity-100 scale-100"
-              To: "transform opacity-0 scale-95"
-          --> */}
-                <div
-                  className={`${
-                    userMenu ? "block" : "none"
-                  } absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none`}
-                  role="menu"
-                  aria-orientation="vertical"
-                  aria-labelledby="user-menu-button"
-                  tabIndex="-1"
-                >
-                  {/* <!-- Active: "bg-gray-100", Not Active: "" -->  */}
-                  <Link
-                    href="/"
-                    className="block px-4 py-2 text-sm text-gray-700"
-                    role="menuitem"
-                    tabIndex="-1"
-                    id="user-menu-item-0"
-                  >
-                    Your Profile
-                  </Link>
-                  <Link
-                    href="/"
-                    className="block px-4 py-2 text-sm text-gray-700"
-                    role="menuitem"
-                    tabIndex="-1"
-                    id="user-menu-item-1"
-                  >
-                    Settings
-                  </Link>
-                  <Link
-                    href="/"
-                    className="block px-4 py-2 text-sm text-gray-700"
-                    role="menuitem"
-                    tabIndex="-1"
-                    id="user-menu-item-2"
-                  >
-                    Sign out
-                  </Link>
-                </div>
+                {/* Profile Menu End  */}
               </div>
             </div>
           </div>
